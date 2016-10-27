16f3684a9ce4f1ff775f55821710f4a6ba0e688d
Shawn Allen <shawn.allen@gsa.gov>
2016-10-27 10:37:43 -0700

    remind myself to look into setting node.parent

--- format/factory.js
+++ format/factory.js
@@ -10,6 +10,7 @@ const factory = (formatters) => {
     var format = formats[node.type];
     switch (typeof format) {
       case 'function':
+        // XXX set node.parent here?
         return format.call(formats, node, parent);
       default:
         throw new Error('Unsupported node type found: "' + node.type + '"');
