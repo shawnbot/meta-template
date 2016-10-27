bb64533d05593f99a4a8e968c530165eae75f4f7
Shawn Allen <shawn.allen@gsa.gov>
2016-10-26 23:07:43 -0700

    reorg format specs

--- test/format.spec.js
+++ test/format.spec.js
@@ -12,40 +12,36 @@ const assertFormats = (fmt, templates) => {
   });
 };
 
-describe('format()', function() {
+describe('default format (nunjucks -> nunjucks)', function() {
 
-  describe('can format an AST back to text', function() {
+  describe('formats output expressions', function() {
+    assertFormats(format(), [
+      "foo {{ bar }} baz",
+      "foo {{ bar }} baz {{ qux[0] }}",
+      "foo {{ bar['baz qux'][1].x }}",
+    ]);
+  });
 
-    describe('formats output expressions', function() {
-      assertFormats(format(), [
-        "foo {{ bar }} baz",
-        "foo {{ bar }} baz {{ qux[0] }}",
-        "foo {{ bar['baz qux'][1].x }}",
-      ]);
-    });
+  describe('formats for..in loops', function() {
+    assertFormats(format(), [
+      "{% for x in items %}la {{ x[0] }}{% endfor %}",
+      "{% for x in items.x['foo bar'].qux %}la {{ x[0] }}{% endfor %}",
+    ]);
+  });
 
-    describe('formats for..in loops', function() {
-      assertFormats(format(), [
-        "{% for x in items %}la {{ x[0] }}{% endfor %}",
-        "{% for x in items.x['foo bar'].qux %}la {{ x[0] }}{% endfor %}",
-      ]);
-    });
+  describe('formats if conditionals', function() {
+    assertFormats(format(), [
+      "{% if z %}yes{% endif %}",
+      "{% if z == 'bar' %}yes{% endif %}",
+      "{% if z %}yes{% else %}no{% endif %}",
+    ]);
+  });
 
-    describe('formats if conditionals', function() {
-      assertFormats(format(), [
-        "{% if z %}yes{% endif %}",
-        "{% if z == 'bar' %}yes{% endif %}",
-        "{% if z %}yes{% else %}no{% endif %}",
-      ]);
-    });
-
-    xdescribe('flattens if/elseif/else hierarchies', function() {
-      assertFormats(format(), [
-        "{% if a %}1{% elseif b %}2{% else %}3{% endif %}",
-        "{% if a %}1{% elseif b %}2{% elseif c %}3{% else %}4{% endif %}",
-      ]);
-    });
-
+  xdescribe('flattens if/elseif/else hierarchies', function() {
+    assertFormats(format(), [
+      "{% if a %}1{% elseif b %}2{% else %}3{% endif %}",
+      "{% if a %}1{% elseif b %}2{% elseif c %}3{% else %}4{% endif %}",
+    ]);
   });
 
 });
