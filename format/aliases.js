8370299ed8216434fa0abcf5fbf005fee7fcfaef
Shawn Allen <shawn.allen@gsa.gov>
2016-11-11 20:11:57 -0800

    add format alias support

---
+++ format/aliases.js
@@ -0,0 +1,9 @@
+/**
+ * This object defines alias mappings for various formats, including
+ * well-known filename extensions.
+ */
+module.exports = {
+  hbs:    'handlebars',
+  jekyll: 'liquid',
+  njk:    'nunjucks',
+};
