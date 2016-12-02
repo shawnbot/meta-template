518353fedde65909879c893fa3da01a735f92a22
Shawn Allen <shawn.allen@gsa.gov>
2016-12-01 17:04:54 -0800

    convert ERB tests to YAML

--- test/run.js
+++ test/run.js
@@ -4,39 +4,60 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const parse = require('../parse');
 
+const opts = {
+  clean: true
+};
+
+const parseString = str => parse.string(str, opts);
+
 const runSpec = (filename, format) => {
   const spec = yaml.safeLoad(fs.readFileSync(filename));
 
   assert(Array.isArray(spec.sections),
          'YAML spec sections should be an array');
 
-  spec.sections.forEach(section => {
-    describe(section.name, function() {
-      if (section.tests) {
-        testConversions(section.tests, format);
+  testSections(spec.sections, format);
+};
+
+const testSections = (sections, format) => {
+  sections.forEach(section => {
+    const desc = section.pending ? xdescribe : describe;
+    desc(section.name, function() {
+      if (section.converts) {
+        testConversions(section.converts, format);
       }
       if (section.invalid) {
         testInvalid(section.invalid, format);
       }
+      if (section.sections) {
+        testSections(section.sections, format);
+      }
     });
   });
 };
 
-const testConversions = (tests, format) => {
-  Object.keys(tests).forEach(input => {
-    const output = tests[input];
+const testConversions = (conv, format) => {
+  const test = (input, output) => {
     it([input, output].join(' => '), function() {
       assert.equal(
-        format(parse.string(input)),
+        format(parseString(input)),
         output
       );
     });
-  });
+  };
+
+  if (Array.isArray(conv)) {
+    conv.forEach(io => test(io.input, io.output));
+  } else {
+    Object.keys(conv).forEach(input => {
+      test(input, conv[input]);
+    });
+  }
 };
 
 const testInvalid = (exprs, format) => {
   exprs.forEach(expr => {
-    assert.throws(() => format(parse.string(expr)));
+    assert.throws(() => format(parseString(expr)));
   });
 };
 
