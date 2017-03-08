1764daaaf4ea96aa778fcdd8855a875326f90830
Shawn Allen <shawn.allen@gsa.gov>
2017-03-07 23:57:41 -0800

    remove duplicate describe() for format tests

--- test/jinja.spec.js
+++ test/jinja.spec.js
@@ -3,9 +3,7 @@ const format = require('../format');
 const path = require('path');
 const runSpec = require('./lib/run-spec');
 
-describe('Jinja2 ("jinja") format', function() {
-  runSpec(
-    path.join(__dirname, 'jinja.spec.yml'),
-    format.jinja
-  );
-});
+runSpec(
+  path.join(__dirname, 'jinja.spec.yml'),
+  format.jinja
+);
